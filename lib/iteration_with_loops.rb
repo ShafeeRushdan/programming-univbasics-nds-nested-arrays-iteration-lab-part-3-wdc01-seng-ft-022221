164031a04d00e74a690eca4cf37423d37d9a6a72
Shafee Rushdan <shafeerushdan@yahoo.com>
2021-02-12 17:20:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,5 @@
- require pry 
-
 def join_nested_strings(mixed_data)
-  binding.pry 
+  
   message = ""
   row_index = 0 
   
@@ -11,27 +9,26 @@ def join_nested_strings(mixed_data)
      
   while element_index < mixed_data.count do 
     
+   puts "bob" 
     
+   if mixed_data[row_index][element_index].class == String.class 
+   
+  message << mixed_data[row_index][element_index]
+ #binding.pry
     
-   if mixed_data[row_index][element_index].class
-
- 
-
-mixed_data[row_index][element_index]
-
 end
-message = mixed_data[row_index][element_index]
+
 end 
-  element_index += 1 
+
   
  row_index += 1
  
- 
+end
+
+message
+
+end
+
  
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
-end
-
-#message
-
-end
